0f6d5e4453921b1d3f998f61c518e3115f1788d7
Ayushhrajput <ayushrajput939@gmail.com>
2026-06-11 21:54:06 +0530

    style: update onClick handler in Profile component for improved readability

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -29,9 +29,9 @@ function Profile(props) {
             {
                 setting &&
                 <div 
-                    onClick={(e) => (
+                    onClick={(e) => {
                         e.stopPropagation()
-                    )}
+                    }}
                     className={`fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 ${darkTheme? " bg-black/10 border-black/10": "bg-white/10 border-white/20"} backdrop-blur-2xl shadow-xl border  flex flex-col justify-center items-center w-max  px-2 rounded-xl`}
                 >
 
